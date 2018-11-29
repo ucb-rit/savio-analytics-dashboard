625dd3adde28e61b8fbdfeb57039ac90cf2a89f6
Nicolas Chan <nicolaschan@berkeley.edu>
2018-11-29 13:08:04 -0800

    Faster lookup times

--- projects/import.sql
+++ projects/import.sql
@@ -1,3 +1,8 @@
 .mode csv
 .import "projects-cleaned.csv" projects
 .import "requests-cleaned.csv" requests
+CREATE INDEX IF NOT EXISTS projects_name ON projects ("Project Name");
+CREATE INDEX IF NOT EXISTS requests_email ON requests ("PI Contact Email Address");
+CREATE TABLE IF NOT EXISTS lookup AS SELECT DISTINCT projects."Project Name" AS project, projects."Faculty Email" AS email, requests."PI's Campus Division or Department" AS department FROM projects, requests WHERE projects."Faculty Email" = requests."PI Contact Email Address";
+CREATE INDEX IF NOT EXISTS lookup_project ON lookup (project);
+
--- projects/lookup.sh
+++ projects/lookup.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)
 PROJECT=$1
-SQL_STATEMENT="select projects.\"Project Name\", projects.\"Faculty Email\", requests.\"PI's Campus Division or Department\" from projects, requests where projects.\"Faculty Email\" = requests.\"PI Contact Email Address\" and projects.\"Project Name\" = \"$1\" LIMIT 1;"
+SQL_STATEMENT="SELECT project, email, department FROM lookup WHERE project = \"$1\";"
 
 RESULT=$(sqlite3 $BASEDIR/db.sqlite3 "$SQL_STATEMENT")
 

2fac747a550031bd148ee14d71fb06764bf40b71
Nicolas Chan <nicolaschan@berkeley.edu>
2019-01-04 13:52:08 -0800

    Change path to database file

--- projects/lookup.sh
+++ projects/lookup.sh
@@ -4,6 +4,6 @@ BASEDIR=$(dirname $0)
 PROJECT=$1
 SQL_STATEMENT="SELECT project, email, department FROM lookup WHERE project = \"$1\";"
 
-RESULT=$(sqlite3 $BASEDIR/db.sqlite3 "$SQL_STATEMENT")
+RESULT=$(sqlite3 /global/home/groups/scs/telegraf/db.sqlite3 "$SQL_STATEMENT")
 
 echo -n $RESULT

2a894404a4d1604af18301cce7474cd4ecc55aeb
Nicolas Chan <nicolaschan@berkeley.edu>
2019-01-23 14:08:37 -0800

    Fix ./ in front of BASEDIR

--- projects/show-jobs.sh
+++ projects/show-jobs.sh
@@ -20,7 +20,7 @@ while IFS='|' read -a LINE; do
   fi
   END_TIMESTAMP="$(date "+%s" --date="${LINE[2]}")000000000"
   IFS='_' read -a TYPE <<< $ACCOUNT
-  IFS='|' read -a DEPARTMENT <<< $(./$BASEDIR/lookup.sh "$ACCOUNT")
+  IFS='|' read -a DEPARTMENT <<< $($BASEDIR/lookup.sh "$ACCOUNT")
   IFS=' ' read -a STATE <<< "${LINE[7]}"
   echo "jobs,job_id=${LINE[0]},account=${ACCOUNT},type=${TYPE[0]},department=$(printf '%q' "${DEPARTMENT[2]}"),cpus=${LINE[4]},partition=${LINE[5]},state=${STATE[0]} raw_time=${LINE[8]},cpu_time=${LINE[9]} $END_TIMESTAMP"
 done

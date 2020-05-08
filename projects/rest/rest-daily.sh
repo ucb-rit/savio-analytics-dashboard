4e9e7fed2b483f43d8ba736dccd06c9c7c6cb9c8
Nicolas Chan <nicolaschan@berkeley.edu>
2020-05-08 12:21:06 -0700

    Update rest-daily.sh

--- projects/rest/rest-daily.sh
+++ projects/rest/rest-daily.sh
@@ -7,7 +7,7 @@ source /home/nicolaschan/.bashrc
 conda activate dashboard
 
 # Collect job data
-$BASEDIR/load-jobs-today-rest.sh
+# $BASEDIR/load-jobs-today-rest.sh
 
 # Collect CPU data
 $BASEDIR/cpu2rest.py
